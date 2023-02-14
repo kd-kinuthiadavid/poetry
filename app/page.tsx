1d78d599e0d492f3798bd1c1cd96ededf6a16ddb
KINUTHIA DAVID <kd.kinuthiadavid@gmail.com>
2023-02-14 21:23:34 +0300

    fix: make improvements to the landing page
    - rename the heading to 'software & soul'
    - fix issues with font

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 export default function Home() {
   return (
-    <main className="flex flex-col lg:flex-row justify-center gap-x-96 items-center h-full gap-y-10 text-left">
-      <div className="flex flex-col gap-y-10">
-        <h1 className="heading">Software & Soul.</h1>
-        <h2 className="text-4xl md:text-6xl font-thin max-w-xl">
-          An exploration of life and human experience through code and poetry.
-        </h2>
-        <small className="font-thin text-xl md:text-2xl max-w-xl leading-relaxed">
-          {/* Hello there, my is{" "}
-          <span className="text-pr-orange">David Kinuthia.</span> Am a software
-          engineer by day and poet by night.{" "} */}
-          <span className="text-pr-orange underline font-medium">
-            Subscribe
-          </span>{" "}
-          to my weekly newsletter of refreshing poetry and short stories.
-        </small>
+    <main className="flex flex-col justify-center lg:flex-row lg:justify-between gap-x-96 items-center h-full gap-y-10 text-left mx-28">
+      <div className="flex flex-col gap-y-10 justify-center items-center text-center lg:text-left">
+        <h1 className="heading">
+          software <br /> & soul.
+        </h1>
+        <div className="lg:ml-16 flex flex-col gap-y-5">
+          <h2 className="text-3xl font-thin max-w-xl leading-tight">
+            An exploration of life and human experience through code and poetry.
+          </h2>
+          <small className="font-thin text-xl max-w-xl leading-relaxed">
+            <span className="text-pr-orange underline font-medium">
+              Subscribe
+            </span>{" "}
+            to my weekly newsletter.
+          </small>
+        </div>
       </div>
       <div className="flex justify-start text-left lg:flex-col gap-x-5 gap-y-2">
         <p className="font-thin underline md:text-2xl cursor-pointer">
