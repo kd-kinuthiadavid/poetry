873811b227254e9e298dc6014c870be1bd7a31df
KINUTHIA DAVID <kd.kinuthiadavid@gmail.com>
2023-02-15 07:19:40 +0300

    fix: add link to the subscribe text on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex flex-col justify-center lg:flex-row lg:justify-between gap-x-96 items-center h-full gap-y-10 text-left mx-28">
@@ -5,14 +7,16 @@ export default function Home() {
         <h1 className="heading">
           software <br /> & soul.
         </h1>
-        <div className="lg:ml-16 flex flex-col gap-y-5">
+        <div className="lg:ml-32 flex flex-col gap-y-5">
           <h2 className="text-3xl font-thin max-w-xl leading-tight">
             An exploration of life and human experience through code and poetry.
           </h2>
           <small className="font-thin text-xl max-w-xl leading-relaxed">
-            <span className="text-pr-orange underline font-medium">
-              Subscribe
-            </span>{" "}
+            <Link href="/subscribe">
+              <span className="text-pr-orange underline font-medium">
+                Subscribe
+              </span>{" "}
+            </Link>
             to my weekly newsletter.
           </small>
         </div>
